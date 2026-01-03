71c1366eab8fdd7764b2dcaa9bc3c7a930b61ced
PratherConid <syxx2013050321@163.com>
2026-01-02 23:56:35 -0800

    initial commit

---
+++ LeanSmtTest.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `LeanSmtTest` library.
+-- Import modules here that should be built as part of the library.
+import LeanSmtTest.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import LeanSmtTest
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
